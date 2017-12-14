369a02716f7d848527a1d0080f03b262dc7d0ffb
JuanGSOT <10orangeblue@gmail.com>
2017-12-14 03:25:42 -0600

    regex email

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-         has_many :registries              
+  has_many :registries
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+                    
 end
