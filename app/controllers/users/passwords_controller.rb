92113dab79d55fbed66574e62645cb21a0fa3c1f
JuanGSOT <10orangeblue@gmail.com>
2017-12-16 19:25:51 -0600

    fix 4

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,5 +1,5 @@
 class Users::PasswordsController < Devise::PasswordsController
-  prepend_before_action :check_captcha, only: [:update]
+  #prepend_before_action :check_captcha, only: [:update]
   # GET /resource/password/new
   # def new
   #   super
@@ -30,13 +30,13 @@ class Users::PasswordsController < Devise::PasswordsController
   # def after_sending_reset_password_instructions_path_for(resource_name)
   #   super(resource_name)
   # end
-  private
+  # private
 
-  def check_captcha
-    unless verify_recaptcha
-      self.resource = resource_class.new
-      resource.validate # Look for any other validation errors besides Recaptcha
-      respond_with_navigational(resource) { render :new }
-    end
-  end
+  # def check_captcha
+  #   unless verify_recaptcha
+  #     self.resource = resource_class.new
+  #     resource.validate # Look for any other validation errors besides Recaptcha
+  #     respond_with_navigational(resource) { render :new }
+  #   end
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20171123000051) do
     t.integer "code"
     t.string "name"
     t.string "surnames"
-    t.boolean "contract"
+    t.boolean "contract", default: false
     t.boolean "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -79,6 +79,10 @@ ActiveRecord::Schema.define(version: 20171123000051) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "name"
     t.string "permission_level"
     t.datetime "created_at", null: false
