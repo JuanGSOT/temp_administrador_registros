5404403f3f86dbc7b048538d7921264aab4a3bc3
JuanGSOT <10orangeblue@gmail.com>
2017-12-18 07:56:18 -0600

    departments?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
   config.read_encrypted_secrets = true
 
   # Disable serving static files from the `/public` folder by default since
+  config.serve_static_files = true
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
